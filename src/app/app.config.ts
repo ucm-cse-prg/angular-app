34c22cb66752bdb1ccf182ead93beaf8f9a371d0
Michael Pisman <mpisman@ucmerced.edu>
2025-04-29 01:44:47 -0700

    feat: Enhance app configuration with dynamic API URL initialization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,31 +1,44 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { httpErrorInterceptor } from '@core/http-error.interceptor';
+import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-const httpInterceptors = [
-    {
-        provide: HTTP_INTERCEPTORS,
-        useClass: httpErrorInterceptor,
-        multi: true
-    },
-    // {
-    //     provide: HTTP_INTERCEPTORS,
-    //     useClass: loggingInterceptor,
-    //     multi: true
-    // }
-];
+import { tap, of, catchError } from 'rxjs';
+import { ConfigService } from '@services/config.service';
+import { environment } from '@environments/environment';
+import { httpErrorInterceptor } from '@core/http-error.interceptor';
+import { routes } from './app.routes';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 export const appConfig: ApplicationConfig = {
     providers: [
+        provideAppInitializer(() => {
+            const settingsService = inject(ConfigService);
+            const httpClient = inject(HttpClient);
+            if (environment.production) {
+                httpClient.get<{ API_URL: string }>('./config.json').pipe(
+                    tap(settings => {
+                        settingsService.apiUrl = settings.API_URL;
+                        return of(true);
+                    }),
+                    catchError(() => {
+                        settingsService.apiUrl = 'http://localhost:8000';
+                        return of(false);
+                    })
+                ).subscribe();
+            } else {
+                settingsService.apiUrl = environment.apiUrl;
+            }
+        }),
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        provideHttpClient(withInterceptorsFromDi()), // DI-based interceptors must be explicitly enabled.
-        httpInterceptors,
-        provideAnimationsAsync(), // Enable async animations
+        provideHttpClient(withInterceptorsFromDi()),
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: httpErrorInterceptor,
+            multi: true
+        },
+        provideAnimationsAsync(),
+        MatSnackBarModule,
     ]
 };
 
